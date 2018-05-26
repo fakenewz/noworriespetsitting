9d0e28b070ca7ab9135fdf82a43889b87d8378a7
popstar123 <wwu3@saic.edu>
2018-05-26 14:44:21 -0500

    revised

--- App/Config/connections.js
+++ App/Config/connections.js
@@ -4,9 +4,9 @@ var source = {
     localhost: {
       host: "localhost",
       port: 3306,
-      user: "root",
-      password: "password",
-      database: "starwars"
+      user: "petapp",
+      password: "noworries",
+      database: ""
     }
   };
   
