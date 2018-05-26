def366c9ed51dbb794204cd7e0d9afd596fe2b10
popstar123 <wwu3@saic.edu>
2018-05-26 15:22:26 -0500

    Changes

--- App/Routes/api-routes.js
+++ App/Routes/api-routes.js
@@ -1,52 +1,45 @@
-// Dependencies
-// =============================================================
-var Character = require("../models/character.js");
 
-// Routes
-// =============================================================
+var petSitters = require("../Models/sitters");
+
 module.exports = function(app) {
-  // Search for Specific Character (or all characters) then provides JSON
-  app.get("/api/:characters?", function(req, res) {
-    // If the user provides a specific character in the URL...
-    if (req.params.characters) {
-      // Then display the JSON for ONLY that character.
-      // (Note how we're using the ORM here to run our searches)
-      Character.findOne({
+  app.get("/api/:petsitters?", function(req, res) {
+    if (req.params.petsitters) {
+      petSitters.findOne({
         where: {
-          routeName: req.params.characters
+          routeName: req.params.petsitters
         }
       }).then(function(result) {
         return res.json(result);
       });
     }
     else {
-      // Otherwise...
-      // Otherwise display the data for all of the characters.
-      // (Note how we're using Sequelize here to run our searches)
-      Character.findAll({}).then(function(result) {
+      petSitters.findAll({}).then(function(result) {
         return res.json(result);
       });
     }
   });
 
-  // If a user sends data to add a new character...
   app.post("/api/new", function(req, res) {
-    // Take the request...
-    var character = req.body;
+    var sitter = req.body;
 
     // Create a routeName
-
     // Using a RegEx Pattern to remove spaces from character.name
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    var routeName = character.name.replace(/\s+/g, "").toLowerCase();
+    var routeName = sitter.name.replace(/\s+/g, "").toLowerCase();
 
     // Then add the character to the database using sequelize
-    Character.create({
+    petSitters.create({
       routeName: routeName,
-      name: character.name,
-      role: character.role,
-      age: character.age,
-      forcePoints: character.forcePoints
+      firstname: sitter.first_name,
+      lastname: sitter.last_name,
+      city: sitter.city,
+      email: sitter.email, 
+      yearsexperience: sitter.years_of_experience,
+      sitdog: sitter.would_sit_dog,
+      sitcat: sitter.would_sit_cat,
+      sitbird: sitter.would_sit_bird,
+      sitsnake: sitter.would_sit_snake,
+      addcomments: sitter.additional_comments,
+
     });
   });
 };
