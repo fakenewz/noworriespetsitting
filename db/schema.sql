a0a108ad001e58483ceb15fbc450a80fbd62d3cd
popstar123 <wwu3@saic.edu>
2018-05-26 18:12:54 -0500

    Changes

--- db/schema.sql
+++ db/schema.sql
@@ -4,4 +4,22 @@ CREATE DATABASE petSitter;
 
 USE petSitter;
 
+CREATE TABLE sitters (
+
+	id int NOT NULL AUTO_INCREMENT,
+	routeName varchar(50) NOT NULL,
+    full_name varchar(50) NOT NULL,
+	city varchar(50) NOT NULL,
+	email varchar(50) NOT NULL,
+	years_of_experience int NOT NULL,
+	would_sit_dog BOOLEAN NOT NULL,
+	would_sit_cat BOOLEAN NOT NULL,
+	would_sit_bird BOOLEAN NOT NULL,
+	would_sit_snake BOOLEAN NOT NULL,
+	additional_comments varchar (280),
+	PRIMARY KEY (id)
+);
+
+
+
 
