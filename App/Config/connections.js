7676eba09f13d689e5a13fece59e87b3a7c1f413
popstar123 <wwu3@saic.edu>
2018-05-26 15:07:58 -0500

    Revised

--- App/Config/connections.js
+++ App/Config/connections.js
@@ -1,28 +1,16 @@
 
-// Setting up our connection information
-var source = {
-    localhost: {
-      host: "localhost",
-      port: 3306,
-      user: "petapp",
-      password: "noworries",
-      database: ""
-    }
-  };
-  
-  
-  // Creating our connection
-  var connection = mysql.createConnection(source.localhost);
-  
-  
-  // Connecting to the database.
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-  
-  // Exporting our connection
-  module.exports = connection;
+var Sequelize = require("sequelize");
+
+var sequelize = new Sequelize("petSitter", "petapp", "noworries", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+module.exports = sequelize;
+
