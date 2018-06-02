e509853cab56b6aea126c20f06be3165d47c10d3
popstar123 <wwu3@saic.edu>
2018-06-02 14:43:08 -0500

    Changed

--- App/Controllers/authcontroller.js
+++ App/Controllers/authcontroller.js
@@ -1,9 +1,21 @@
 var exports = module.exports = {}
-var path = require('path')
  
 exports.signup = function(req, res) {
- 
-    res.render(path.join(__dirname, "../Public/2. Pages/9. Account/login.hbs"),{});
+    res.render('signup');
+}
+
+exports.signin = function(req, res) {
+    res.render('signin');
+}
+
+exports.dashboard = function(req, res) {
+    res.render('dashboard');
+}
+
+exports.logout = function(req, res) {
+    req.session.destroy(function(err) {
+        res.redirect('/');
+    });
  
 }
 
--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require("express"); //
+var bodyParser = require("body-parser"); //
 
-var env = require('dotenv').load();
-var exphbs = require('express-handlebars')
+var env = require('dotenv').load(); //
+var exphbs = require('express-handlebars') //
 
-var passport = require('passport');
-var session = require('express-session');
+var passport = require('passport'); //
+var session = require('express-session'); //
 
-var app = express();
+var app = express(); //
 var PORT = process.env.PORT || 8080;
 
-var db = require("./App/Models")
+var db = require("./App/Models") //
 
-app.use(session({ secret: 'secret',resave: true, saveUninitialized:true})); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-
-
-app.set('9. Account', './App/Public/2. Pages/9. Account') //
-app.engine('hbs', exphbs({
-    extname: '.hbs'
-}));
-app.set('view engine', '.hbs');
-
-var authroutes = require("./App/Routes/auth")(app)
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false})); // session secret
+app.use(passport.initialize()); //
+app.use(passport.session()); //
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.set('views', './App/views') //
+app.engine('hbs', exphbs({ //
+    extname: '.hbs' //
+})); //
+app.set('view engine', '.hbs'); //
+
+var authRoute = require("./App/Routes/auth")(app, passport); //
+
+require('./App/Config/passport/passport')(passport, db.users); //
+
+app.use(bodyParser.urlencoded({ extended: true })); //
+app.use(bodyParser.json()); //
 
 app.use(express.static("app/public"));
 
